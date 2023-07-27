b932c29c9dcdf86752d51f0527042094bd637a6c
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2023-07-27 11:20:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,30 @@ import math
 exp = 0
 level = 1
 exp_needed = 0
+total_exp = 0
 
-
-def level_curve():
+def level_curve(level):
     exp_needed = pow(level,1.5) + (level * 100)
-    print(exp_needed)
+    #print(exp_needed)
+    return exp_needed
 
 def level_up():
     global level
     level += 1
     print(level)
+    return level
 
 
+def total_exp_needed():
+    global total_exp
+    global level
+    levelstart = 1
+    for levelstart in range(level+1):
+        total_exp = total_exp + level_curve(levelstart)
+        levelstart = levelstart + 1
+    print(total_exp)
 
-level_curve()
+
+level_curve(level)
 level_up()
-level_curve()
+total_exp_needed()
